61f35e8efb0afa39672db48e1b4f3c97f56bfda5
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-24 18:59:30 +0100

    DataContextEF and UsersController setup

---
+++ T072_RepositoryFlow/Data/DataContextEF.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using T072_RepositoryFlow.Models;
+
+namespace T072_RepositoryFlow.Data
+{
+	public class DataContextEF(IConfiguration config) : DbContext
+	{
+		private readonly IConfiguration _config = config;
+
+		public virtual DbSet<UserModel> Users { get; set; }
+		public virtual DbSet<UserSalaryModel> UserSalary { get; set; }
+		public virtual DbSet<UserJobInfoModel> UserJobInfo { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if (!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder
+					.UseSqlServer(_config.GetConnectionString("DefaultConnection"),
+						optionsBuilder => optionsBuilder.EnableRetryOnFailure());
+			}
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.HasDefaultSchema("TutorialAppSchema");
+
+			modelBuilder.Entity<UserModel>()
+				.ToTable("Users", "TutorialAppSchema")
+				.HasKey(u => u.UserId);
+
+			modelBuilder.Entity<UserSalaryModel>()
+				.HasKey(u => u.UserId);
+			modelBuilder.Entity<UserJobInfoModel>()
+				.HasKey(u => u.UserId);
+		}
+	}
+}
