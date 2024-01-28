4d95043ee81b896bda24f84de599a47078f1b9f9
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-28 10:03:46 +0100

    Auth settings and Models

---
+++ T075_Security/Dtos/UserForLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace T075_Security.Dtos
+{
+	public class UserForLoginDto
+	{
+		public string Email { get; set; } = "";
+		public string Password { get; set; } = "";
+	}
+}
---
+++ T075_Security/Dtos/UserForRegistrationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace T075_Security.Dtos
+{
+	public class UserForRegistrationDto
+	{
+		public string Email { get; set; } = "";
+		public string Password { get; set; } = "";
+		public string PasswordConfirm { get; set; } = "";
+	}
+}
