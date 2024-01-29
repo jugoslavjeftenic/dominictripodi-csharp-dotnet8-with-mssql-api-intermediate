e09a229929a884f97979a3ca9a8e630d5069edd3
Jugoslav Jeftenic <jugoslav.jeftenic.su@gmail.com>
2024-01-29 18:19:52 +0100

    JWT creation

--- T075_Security/Controllers/AuthController.cs
+++ T075_Security/Controllers/AuthController.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.IdentityModel.Tokens;
 using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
 using T075_Security.Data;
@@ -11,7 +14,6 @@ namespace T075_Security.Controllers
 {
 	public class AuthController(IConfiguration config) : ControllerBase
 	{
-
 		private readonly DataContextDapper _dapper = new(config);
 		private readonly IConfiguration _config = config;
 
@@ -122,7 +124,16 @@ namespace T075_Security.Controllers
 				}
 			}
 
-			return Ok();
+			string sqlUserId = @$"
+			SELECT
+				[UserId]
+			FROM TutorialAppSchema.Users 
+			WHERE Email = '{userForLogin.Email}'
+			";
+
+			int userId = _dapper.LoadDataSingle<int>(sqlUserId);
+
+			return Ok(new Dictionary<string, string> { { "token", CreateToken(userId) } });
 		}
 
 		private byte[] GetPasswordHash(string password, byte[] passwordSalt)
@@ -135,9 +146,32 @@ namespace T075_Security.Controllers
 				password: password,
 				salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
 				prf: KeyDerivationPrf.HMACSHA256,
-				iterationCount: 100000,
+				iterationCount: 1000000,
 				numBytesRequested: 256 / 8
 			);
+		}
+
+		private string CreateToken(int userId)
+		{
+			Claim[] claims = [new("userId", userId.ToString())];
+
+			string? tokenKeyString = _config.GetSection("AppSettings:TokenKey").Value;
+			SymmetricSecurityKey tokenKey = new(Encoding.UTF8.GetBytes(tokenKeyString ?? ""));
+
+			SigningCredentials credentials = new(tokenKey, SecurityAlgorithms.HmacSha512Signature);
+
+			SecurityTokenDescriptor descriptor = new()
+			{
+				Subject = new ClaimsIdentity(claims),
+				SigningCredentials = credentials,
+				Expires = DateTime.Now.AddDays(1)
+			};
+
+			JwtSecurityTokenHandler tokenHandler = new();
+
+			SecurityToken token = tokenHandler.CreateToken(descriptor);
+
+			return tokenHandler.WriteToken(token);
 		}
 	}
 }
